34b0c4cc8286abd78c33708ff8e43f21204e41ca
Bantu-Bethu Beya <90983460+ChocTrippleB@users.noreply.github.com>
2026-05-22 11:37:19 +0200

    Round-Robin logic
    Built the round-robin logic, for return a different server for a new request. This allows for the requests to be distributed evenly across servers.
    
    The mu.Lock, is there to make sure that only one goroutine executes, this also is applied to a singleton lock for thread safety.  The defer mu.Unlock(), is more like a scheduler for a function to run before the end no matter what happens.
    
    The round-robin occurs at the current = ...; we return a new server each time.
    
    Handling the request, proxy acts as the reverse proxy that forwards the request to the available server. Note to myself, go does not have exception handling like my primary language C#, so we use and inline style: url, err = ...

--- main.go
+++ main.go
@@ -2,12 +2,39 @@ package main
 
 import (
     "fmt"
-    "net/http"
-	"net/http/httptil"
-	"net/url"
-	"sync"
+    "net/http" // handles HTTP requests/responses
+	"net/http/httputil" // reverse proxy, forwards traffic to backend servers. In load-balancers
+	"net/url" // Used for working with URLs.
+	"sync" // For concurrency safety (threads) 'go'
 )
 
+var servers = []string{
+	"http://localhost:8081",
+	"http://localhost:8082",
+	"http://localhost:8083",
+}
+
+// Mutex to protect access to the current server index.
+// "Only one person can enter this room at once."
+var mu sync.Mutex
+var current int
+
+func getNextServer() string {
+	mu.Lock() //only one goroutine can execute this critical section at a time.
+	defer mu.Unlock() // Run this later before the function exits.
+	server := servers[current] //   := variable declaration and assignment in one step. in C# += 
+	current = (current + 1) % len (servers)  // Move to the next server index, get remainder to loop back to the start of the list.
+	return server
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	target := getNextServer()
+	fmt.Println("Forwarding to: ", target)
+
+	url, _ := url.Parse(target) // no try-catch or exceptions in Go.
+	proxy := httputil.NewSingleHostReverseProxy(url)
+	proxy.ServeHTTP(w, r)
+}
 
 
 func main() {
